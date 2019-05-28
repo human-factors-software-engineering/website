4d072f0cc5ea65ecaaa30614208cd2bc7ee57512
Jada A. Houser <47469815+jahouse@users.noreply.github.com>
2019-05-28 15:58:21 -0400

    Added Carousel Responsiveness and More
    Made it so the carousel switches between a large and small picture depending on screen size. Added lower section with proto-team picture and icon stuff. Will add icons and respective text for eventual page links.

---
+++ Website Files/main-js.js
@@ -0,0 +1,14 @@
+/*Switching images based on screen size - code snippet edited from https://stackoverflow.com/a/36402526*/
+var image1 =  $('.carousel-image-1').find("img");
+var image2 =  $('.carousel-image-2').find("img");
+
+function setImages() {
+  var windowWidth = window.innerWidth,
+      src1 = (windowWidth < 768) ? 'images/carousel-pt1-small-screen.svg' : 'images/carousel-pt1.svg';
+      src2 = (windowWidth < 768) ? 'images/carousel-pt2-small-screen.svg' : 'images/carousel-pt2.svg';
+  image1.attr('src', src1);
+  image2.attr('src', src2)
+}
+
+$(window).on('resize', setImages);
+setImages();
