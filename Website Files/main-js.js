71f40a266f208fc43002cb9cf4d77ab9cf2145d0
Jada A. Houser <47469815+jahouse@users.noreply.github.com>
2019-05-31 16:00:25 -0400

    Setup Template for the Profile Pieces
    Setup the template for the profile pieces (picture, name, icons, bios) and made them responsive.
    Next steps:
    1) figure out how to get the icons closer together when they're on a smaller screen,
    2) when a user hovers over the icon, turn the icon gray,
    3) add the rest of the proto-profiles and content,
    4) get help on figuring out how to make a proper header (the one at the top is not up to the task at the moment),
    5) don't forget to make the footer

--- Website Files/main-js.js
+++ Website Files/main-js.js
@@ -4,8 +4,8 @@ var image2 =  $('.carousel-image-2').find("img");
 
 function setImages() {
   var windowWidth = window.innerWidth,
-      src1 = (windowWidth < 768) ? 'images/carousel-pt1-small-screen.svg' : 'images/carousel-pt1.svg';
-      src2 = (windowWidth < 768) ? 'images/carousel-pt2-small-screen.svg' : 'images/carousel-pt2.svg';
+      src1 = (windowWidth < 768) ? 'images/home-page-imgs/carousel-pt1-small-screen.svg' : 'images/home-page-imgs/carousel-pt1.svg';
+      src2 = (windowWidth < 768) ? 'images/home-page-imgs/carousel-pt2-small-screen.svg' : 'images/home-page-imgs/carousel-pt2.svg';
   image1.attr('src', src1);
   image2.attr('src', src2)
 }
