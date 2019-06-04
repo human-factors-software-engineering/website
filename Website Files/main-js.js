8ba14bff5df562e4b33196e2279fb634f9627727
Jada A. Houser <47469815+jahouse@users.noreply.github.com>
2019-06-04 11:01:03 -0400

    Used Javascript for Hover Behavior on Profile Icons
    I figured out way to use JS to do the hover behavior, however, the implementation isn't too efficient.
    
    Hopefully I can figure out an if statement that'll switch icons depending on the class of the incoming object. Even better if I could use an if statement but instead of switching images, toggle between the colors by accessing the SVG elements.
    
    (I also need to change the hover behavior for the github icon on the navbar from CSS to JS.)
    
    Moving on to spacing the profile icons more appropriately now that I've removed the borders...

--- Website Files/main-js.js
+++ Website Files/main-js.js
@@ -7,10 +7,28 @@ function setImages() {
       src1 = (windowWidth < 768) ? 'images/home-page-imgs/carousel-pt1-small-screen.svg' : 'images/home-page-imgs/carousel-pt1.svg';
       src2 = (windowWidth < 768) ? 'images/home-page-imgs/carousel-pt2-small-screen.svg' : 'images/home-page-imgs/carousel-pt2.svg';
   image1.attr('src', src1);
-  image2.attr('src', src2)
+  image2.attr('src', src2);
 }
 
 $(window).on('resize', setImages);
 setImages();
 
-/*Switching icons based on screen size*/
+/*Switch icon color on hover*/
+/*This could be way more efficient...*/
+
+/*-------profWebIcon-------*/
+function mouseOnW(webIcon) {
+  webIcon.src = 'images/people-page-imgs/web-icon-gray.svg';
+}
+
+function mouseOffW(webIcon) {
+  webIcon.src = 'images/people-page-imgs/web-icon-orange.svg';
+}
+/*-------profGitIcon-------*/
+function mouseOnG(gitIcon) {
+  gitIcon.src = 'images/github-logo-small-gray.svg';
+}
+
+function mouseOffG(gitIcon) {
+  gitIcon.src = 'images/github-logo-small-orange.svg';
+}
